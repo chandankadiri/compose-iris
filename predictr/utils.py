ec164692b2c749e39bb3538eec5511d988e5ad26
Chandan K <85396243+chandankadiri@users.noreply.github.com>
2021-08-24 22:13:06 +0530

    Updated with New dataset

--- predictr/utils.py
+++ predictr/utils.py
@@ -2,21 +2,23 @@ import pickle
 from sklearn.naive_bayes import GaussianNB
 
 # define a Gaussain NB classifier
-clf = GaussianNB()
+#clf = GaussianNB()
+clf=None
 
 # define the class encodings and reverse encodings
-classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
+classes = {0: "Adelie", 1: "Chinstrap", 2: "Gentoo"}
 r_classes = {y: x for x, y in classes.items()}
 
 
 # function to load the model
 def load_model():
     global clf
-    clf = pickle.load(open("models/iris_nb.pkl", "rb"))
+    clf = pickle.load(open("models/nb_model.pkl", "rb"))
 
 
 # function to predict the flower using the model
 def predict(query_data):
+    clf=pickle.load(open("models/nb_model.pkl", "rb"))
     x = list(query_data.dict().values())
     prediction = clf.predict([x])[0]
     return classes[prediction]
