f443840c439c72f70c185b5208314c1aaac0668b
Chandan K <85396243+chandankadiri@users.noreply.github.com>
2021-08-24 22:14:21 +0530

    Updated with New dataset

--- predictr/main.py
+++ predictr/main.py
@@ -8,15 +8,15 @@ app = FastAPI(title="predictr", docs_url="/")
 
 # class which is expected in the payload
 class QueryIn(BaseModel):
-    sepal_length: float
-    sepal_width: float
-    petal_length: float
-    petal_width: float
+    culmen_length_mm: float
+    culmen_depth_mm: float
+    flipper_length_mm: float
+    body_mass_g: float
 
 
 # class which is returned in the response
 class QueryOut(BaseModel):
-    flower_class: str
+    penguin_class: str
 
 
 # Route definitions
@@ -26,12 +26,12 @@ def ping():
     return {"ping": "pong"}
 
 
-@app.post("/predict_flower", response_model=QueryOut, status_code=200)
+@app.post("/predict_penguin", response_model=QueryOut, status_code=200)
 # Route to do the prediction using the ML model defined.
 # Payload: QueryIn containing the parameters
 # Response: QueryOut containing the flower_class predicted (200)
-def predict_flower(query_data: QueryIn):
-    output = {"flower_class": predict(query_data)}
+def predict_penguin(query_data: QueryIn):
+    output = {"penguin_class": predict(query_data)}
     return output
 
 
